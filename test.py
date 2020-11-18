4509ce6af7cf7d049848fa2a7948f7c27d5cf6b8
codeperfectplus <deepak008@live.com>
2020-11-18 10:28:22 +0530

    testing

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-import requests
-from PIL import Image
+def main():
+    print('hello world')
+print('guru99')
 
-image_path = '/home/inspiron3551/Downloads/Pictures/30-of-the-most-influential-and-famous-entrepreneurs-from-all-over-the-world.jpg'
-
-url = 'http://127.0.0.1:5000/'
-
-r = requests.post(url, data={'file': open(image_path, 'rb')})
-
-print(r.text)
+# calling the function
+main()
