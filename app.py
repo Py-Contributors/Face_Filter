e9e4466bcc5138b6221b1921a0e3f434ba2be52c
codeperfectplus <deepak008@live.com>
2020-11-18 21:37:50 +0530

    updating api

--- app.py
+++ app.py
@@ -1,49 +1,128 @@
 #!/home/inspiron3551/anaconda3/bin/python
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect, send_file, url_for
 from PIL import Image
 import cv2
+
 from facefilter import main
+from face_detection import detectedFace
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+""" 
+OpenCV FaceFilter RestAPI
+"""
 @app.route('/', methods=['GET'])
 def home():
     return jsonify(
-        {'title': 'OpenCV REST API with Flask',
-        'Face Filter': 'face_filter_url',
-        'Face Detection': 'face_detection_url',
+        {
+            'title': 'OpenCV REST API with Flask',
+        'face filter': 'face_filter_url',
+        'face detection': 'face_detection_url',
         'github': 'https://github.com/codeperfectplus',
         'documentation': 'documentation_url',
-        'author': ' '
-        })
-
+        'author': 'Deepak Raj'
+        }
+    )        
+'''
+Face Filter post request.
+input post;
+    file: image_file
+    mask: num:<1-3>
+'''
 @app.route('/facefilter', methods=['POST'])
-def handleUpload():
+def face_filter():
 
     try:
         os.mkdir('media')
     except Exception:
         print('Folder already exists')
 
-    target = os.path.join(BASE_DIR, 'media')
+    media_root = os.path.join(BASE_DIR, 'media')
     upload_image = request.files.getlist('file')[0]
     mask_num = request.form["mask"]
 
     image_name = upload_image.filename
 
-    destination = "/".join([target, image_name])
+    image_path = "/".join([media_root, image_name])
     
     # saving input image to server
-    upload_image.save(destination)
+    upload_image.save(image_path)
 
-    preprocessed_image = main(destination, mask_num)
+    # main() function is for apply mask on image
+    preprocessed_image = main(image_path, mask_num)
     preprocessed_image = Image.fromarray(preprocessed_image, 'RGB')
-    preprocessed_image.save(destination)
+    preprocessed_image.save(image_path)
 
-    return jsonify({
-        'Image Saved': True,
-        'Image_path': destination
-        })
+    return jsonify(
+        {
+            'output_image': image_path,
+            'file_name': image_name
+        }
+    )
+
+'''
+Face Detection Post Request
+
+Input post request:
+    file: image_file
+output:
+    detected face and number of face
+'''
+@app.route('/facedetection', methods=['post'])
+def face_detection():
+
+    media_root = os.path.join(BASE_DIR, 'media')
+    upload_image = request.files.getlist('file')[0]
+
+    image_name = upload_image.filename
+
+    image_path = "/".join([media_root, image_name])
+    
+    # saving input image to server
+    upload_image.save(image_path)
+
+    # main() function is for apply mask on image
+    preprocessed_image, num_of_faces = detectedFace(image_path)
+    preprocessed_image = Image.fromarray(preprocessed_image, 'RGB')
+    preprocessed_image.save(image_path)
+
+    return jsonify(
+        {
+            'Total detected face': num_of_faces,
+            'output_image': image_path,
+            'file_name': image_name
+        }
+    )
+
+
+
+''' This is only for debugging and testing purpose'''
+@app.route('/test', methods=['post'])
+def test():
+
+    media_root = os.path.join(BASE_DIR, 'media')
+    upload_image = request.files.getlist('file')[0]
+
+    image_name = upload_image.filename
+
+    image_path = "/".join([media_root, image_name])
+    
+    # saving input image to server
+    upload_image.save(image_path)
+
+    # main() function is for apply mask on image
+    preprocessed_image, num_of_faces = detectedFace(image_path)
+    preprocessed_image = Image.fromarray(preprocessed_image, 'RGB')
+    preprocessed_image.save(image_path)
+    
+
+    return jsonify(
+        {
+            'Total detected face': num_of_faces,
+            'output_image': image_path,
+            'file_name': image_name,
+        }
+    )
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 ''' test script locally '''
 import requests
 
-local_url = 'http://localhost:5000/facefilter'
+local_url = 'http://localhost:5000/facedetection'
 heroku_url = 'https://snpachat-filter-restapi.herokuapp.com/facefilter'
 
 file_path = '/home/inspiron3551/Downloads/Pictures/30-of-the-most-influential-and-famous-entrepreneurs-from-all-over-the-world.jpg'
@@ -13,4 +13,4 @@ files = {
 response = requests.post(local_url, files=files)
 print('Respone: ', response)
 print(response.text)
-print(response.json())
+#print(response.json())
