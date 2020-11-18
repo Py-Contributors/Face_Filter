6d7f9f8fee39a8b6f81559546386f93dedb00c49
codeperfectplus <deepak008@live.com>
2020-11-18 13:05:53 +0530

    te4sting

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import requests
 
 local_url = 'http://localhost:5000/facefilter'
 heroku_url = 'https://snpachat-filter-restapi.herokuapp.com/facefilter'
+
 file_path = '/home/inspiron3551/Downloads/Pictures/30-of-the-most-influential-and-famous-entrepreneurs-from-all-over-the-world.jpg'
 
 files = {
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000,debug=True)
+    app.run()
