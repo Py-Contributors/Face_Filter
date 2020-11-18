9b64b86a1738433fda276b652ae3361b69b1500b
codeperfectplus <deepak008@live.com>
2020-11-18 13:00:10 +0530

    test

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 ''' test script locally '''
 import requests
 
+local_url = 'http://localhost:5000/facefilter'
+heroku_url = 'https://snpachat-filter-restapi.herokuapp.com/facefilter'
 file_path = '/home/inspiron3551/Downloads/Pictures/30-of-the-most-influential-and-famous-entrepreneurs-from-all-over-the-world.jpg'
 
 files = {
     'file': open(file_path, 'rb'),
 }
 
-response = requests.post('http://localhost:5000/facefilter', files=files)
+response = requests.post(heroku_url, files=files)
 print(response.json())
