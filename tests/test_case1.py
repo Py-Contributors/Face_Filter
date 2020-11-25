d43e220da364731b6a2243e1edab37c3f3fd6618
codeperfectplus <deepak008@live.com>
2020-11-25 22:13:13 +0530

    minor improvement

--- tests/test_case1.py
+++ tests/test_case1.py
@@ -6,46 +6,47 @@ Test case locally for get request
 """
 from manage import app
 
+client = app.test_client()
 
 def test_app():
     """ Test case for get Request """
-    response = app.test_client().get("/")
+    response = client.get("/")
 
     assert response.status_code == 200
     assert type(response.data) == bytes
 
     """ Test case for face detection version 1 """
-    response = app.test_client().get("api/v1/facedetection")
+    response = client.get("api/v1/facedetection")
 
     assert response.status_code == 200
     assert type(response.data) == bytes
 
     """ Test case for face detection version 2 """
-    response = app.test_client().get("api/v2/facedetection")
+    response = client.get("api/v2/facedetection")
 
     assert response.status_code == 200
     assert type(response.data) == bytes
 
     """ Test case for Face Filter Version 1 """
-    response = app.test_client().get("api/v1/facefilter")
+    response = client.get("api/v1/facefilter")
 
     assert response.status_code == 200
     assert type(response.data) == bytes
 
     """ Test case for get image """
-    response = app.test_client().get("/uploads/sample.jpg")
+    response = client.get("/uploads/sample.jpg")
 
     assert response.status_code == 200
     assert type(response.data) == bytes
 
     """ Test case for show command """
-    response = app.test_client().get("/command/show")
+    response = client.get("/command/show")
 
     assert response.status_code == 200
     assert type(response.data) == bytes
 
     """ Test Case for delete command """
-    response = app.test_client().get("/command/delete")
+    response = client.get("/command/delete")
 
     assert response.status_code == 200
     assert type(response.data) == bytes
--- tests/test_case2.py
+++ tests/test_case2.py
@@ -8,13 +8,15 @@ import os
 from manage import app
 from settings import ASSETS_DIR
 
+client = app.test_client()
+
 file_path = os.path.join(ASSETS_DIR, "sample.jpg")
 
 
 def test_app():
     """ Test case for Face Detection version 1 """
     data = {"file": open(file_path, "rb")}
-    response = app.test_client().post(
+    response = client.post(
         "api/v1/facedetection", data=data, content_type="multipart/form-data"
     )
     assert response.status_code == 200
@@ -23,7 +25,7 @@ def test_app():
     """ Test case for face detection version 2 """
     data = {"file": open(file_path, "rb")}
 
-    response = app.test_client().post(
+    response = client.post(
         "api/v2/facedetection", data=data, content_type="multipart/form-data"
     )
     assert response.status_code == 200
@@ -33,7 +35,7 @@ def test_app():
     data = {"mask": 1}
     data["file"] = open(file_path, "rb")
 
-    response = app.test_client().post(
+    response = client.post(
         "/api/v1/facefilter", data=data, content_type="multipart/form-data"
     )
     assert response.status_code == 200
