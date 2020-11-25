77604b6d95a666099a520fb83f9e7b2da49a6727
codeperfectplus <deepak008@live.com>
2020-11-25 22:02:55 +0530

    minor changes

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 #!/home/inspiron3551/anaconda3/bin/python
-from app import app
 from waitress import serve
 
+from main import app
 
 if __name__ == "__main__":
     print("Starting server...")
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=8000)
