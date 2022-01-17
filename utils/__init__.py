df151e085817acf83def1a4f7e4d899989ff7d6f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-18 01:41:47 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from utils.faceDetection_v1 import faceDetectionv1
-from utils.faceDetection_v2 import faceDetectionv2
-
-from utils.faceFilter_v1 import faceFilterv1
-from utils.faceFilter_v2 import faceFilterv2
